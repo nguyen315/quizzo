0dc64fcbb77e674a7b5c12ecdfb9bae5067096b4
thanhloc1506 <locpooh1506@gmail.com>
2021-07-15 17:47:48 +0700

    modify design form

--- client/src/views/LandingPage.tsx
+++ client/src/views/LandingPage.tsx
@@ -3,9 +3,8 @@ import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-// import { test } from "../store/actions/authActions"
 import { Container } from "react-bootstrap";
-// import { BsFillPersonFill } from "react-icons/bs";
+import "../css/landing.css";
 import QuizzoTitle from "../components/layouts/QuizzoTitle";
 import MyNavbar from "../components/layouts/MyNavbar";
 import PINField from "../components/layouts/PINField";

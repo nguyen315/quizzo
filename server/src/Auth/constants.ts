c2b51f6f435e11fa22e66cad28bf16bbf2067f55
nguyen315 <nguyen315@github.com>
2021-07-15 11:19:42 +0700

    implement JWT and Hash Password
    implement JWT and Hash Password
    
    hide password before return

--- server/src/Auth/constants.ts
+++ server/src/Auth/constants.ts
@@ -1,3 +1,3 @@
 export const jwtConstants = {
-  secret: 'meomeomeo',
+  secret: process.env.JWTSECRET || 'meomeomeo',
 };
--- server/src/User/user.module.ts
+++ server/src/User/user.module.ts
@@ -1,17 +1,14 @@
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
 /*
 https://docs.nestjs.com/modules
 */
-
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
-import { AuthModule } from '../Auth/auth.module';
-import { forwardRef } from '@nestjs/common';
 
 @Module({
-  imports: [forwardRef(() => AuthModule), TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],

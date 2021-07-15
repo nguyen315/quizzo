0824bd1f06870cb38770ee58898d798386c1d61e
nguyen315 <nguyen315@github.com>
2021-07-15 09:01:38 +0700

    init JWT

--- server/src/Auth/auth.service.ts
+++ server/src/Auth/auth.service.ts
@@ -5,12 +5,19 @@ https://docs.nestjs.com/providers#services
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/User/user.entity';
 import { UserService } from 'src/User/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  async validateUser(username: string, pass: string): Promise<User |undefined> {
+  async validateUser(
+    username: string,
+    pass: string,
+  ): Promise<User | undefined> {
     const user = await this.userService.findByUsername(username);
 
     if (user && user.password === pass) {
@@ -18,5 +25,12 @@ export class AuthService {
     }
 
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }

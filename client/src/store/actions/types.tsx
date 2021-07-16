be3546aa6079f49c544e3a8b870d51e39856a4b0
nguyen315 <nguyen315@github.com>
2021-07-16 17:19:13 +0700

    Set GENERATE_SOURMAP & NODE_ENV when build

--- client/src/store/actions/types.tsx
+++ client/src/store/actions/types.tsx
@@ -32,4 +32,4 @@ export type registerForm = {
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:8080/api"
-    : "https://...";
+    : "https://quizzo-service.herokuapp.com";

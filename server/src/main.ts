e3bdf79787f7715879d5ccf63afc3cd2f865a004
nguyen315 <nguyen315@github.com>
2021-07-20 11:03:40 +0700

    Merge from branch HB_0010 to develop: Config env backend

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();

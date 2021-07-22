7cb87d8ca86b4305f6c30175f5518987fa1874cc
Cong Toan Le <congtoanle01@gmail.com>
2021-07-22 14:34:18 +0700

    modified mail service confirm link

--- server/src/mail/mail.service.ts
+++ server/src/mail/mail.service.ts
@@ -6,11 +6,11 @@ export class MailService {
   constructor(private mailerService: MailerService) {}
 
   async sendUserConfirmation(user, token: string) {
-    const url = `outlook.com/auth/confirm?token=${token}`;
+    // const url = `outlook.com/auth/confirm?token=${token}`;
+    const url = `http://localhost:5000/`
 
     await this.mailerService.sendMail({
       to: user.email,
-      // from: '"Support Team" <support@example.com>', // override default from
       from: 'quizzo_game@outlook.com',
       subject: 'Welcome to Nice App! Confirm your Email',
       template: './confirmation', // `.hbs` extension is appended automatically

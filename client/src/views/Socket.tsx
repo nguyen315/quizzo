62653bcd0c6b2bb7dd817db399168f68d29ad08a
nguyen315 <nguyen315@github.com>
2021-07-22 15:10:36 +0700

    Basic socket.io demo

--- client/src/views/Socket.tsx
+++ client/src/views/Socket.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import socketClient from 'socket.io-client';
+import socketClient, { io } from 'socket.io-client';
 import { RootState } from '../store/store';
 import MyNavbar from '../components/layouts/MyNavbar';
 import { apiUrl } from '../store/types';
@@ -8,27 +8,28 @@ import ListMessages from '../components/socket/ListMessages';
 import { Container } from 'react-bootstrap';
 
 const socketUrl = `${apiUrl}/socket`;
-console.log(socketUrl);
+// console.log(socketUrl);
 
 const Socket = () => {
   const auth = useSelector((state: RootState) => state.auth);
 
   const [socket, setSocket] = useState<any>();
-  const [messages, setMessages] = useState<String[]>([
-    'test message 1',
-    'Hello from user 1',
-    'Hello from user 2'
-  ]);
+  const [messages, setMessages] = useState<String[]>([]);
 
   const configureSocket = () => {
-    let thissocket = socketClient('http://localhost:5000/api/', {
-      path: 'socket'
+    const clientSocket = io('http://localhost:5000', {
+      path: '/api/socket',
+      transports: ['websocket']
     });
-    thissocket.on('connection', () => {});
-    thissocket.on('message', (message: String) => {
+
+    clientSocket.on('connect', () => {
+      console.log('connected');
+    });
+    clientSocket.on('message', (message: String) => {
       setMessages((prevState) => prevState.concat(message));
     });
-    setSocket(thissocket);
+
+    setSocket(clientSocket);
   };
 
   const handleSendMessage = () => {
@@ -46,7 +47,7 @@ const Socket = () => {
       <MyNavbar />
       <div>Socket demo </div>
       <button onClick={handleSendMessage}> Send Hello Message</button>
-      <ListMessages messages={messages} />
+      <ListMessages messages={[...messages].reverse()} />
     </Container>
   );
 };

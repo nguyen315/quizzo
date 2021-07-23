b80e27eb1911ff87f5bfd486e87524a87c117e94
ngvuthanhnhan <ngvuthanhnhan@gmail.com>
2021-07-23 10:57:45 +0700

    Update server port to 5000

--- server/src/Room/room.service.ts
+++ server/src/Room/room.service.ts
@@ -30,12 +30,14 @@ export class RoomService {
   async findAll() {
     const users = await this.userRepository.find()
     const response = [];
+    let responseUser = null;
     for (const user of users) {
       let user_id = user.id;
       let rooms = await this.roomRepository.find({
         user_id: user_id
       });
-      response.push({ ...user, rooms: rooms});
+      let { password, ...responseUser } = user;
+      response.push({ ...responseUser, rooms: rooms});
     }
     return response;
   }

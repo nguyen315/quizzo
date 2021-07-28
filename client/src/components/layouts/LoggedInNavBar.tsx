e98a78d14ae39f614621b0cdbe2934e8393b7be2
superman19993 <73101805+superman19993@users.noreply.github.com>
2021-07-28 15:13:21 +0700

    Navbar Component added

---
+++ client/src/components/layouts/LoggedInNavBar.tsx
@@ -0,0 +1,51 @@
+import { connect, useDispatch, useSelector } from 'react-redux';
+import { Container, Dropdown, Nav, Navbar } from 'react-bootstrap';
+import { RootState } from '../../store/store';
+import { logOut, showUpdateModal } from '../../store/slices/auth.slice';
+import { setAuthToken } from '../../utils/setAuthToken';
+
+const LoggedInNavbar: React.FC = (props: any) => {
+  const auth = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch();
+
+  const update = () => {
+    dispatch(showUpdateModal());
+  };
+
+  const logoutUser = () => {
+    setAuthToken(null);
+    localStorage.removeItem('Authorization');
+    dispatch(logOut());
+  };
+
+  return (
+    <>
+      <br />
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand className="NavbarQuizzo" href="#home">
+            Quizzo
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/dashboard">Home</Nav.Link>
+            <Nav.Link href="/list-questions">Questions</Nav.Link>
+            <Nav.Link href="#rooms">Rooms</Nav.Link>
+            <Dropdown>
+              <Dropdown.Toggle variant="success">Account</Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item onClick={update}>Update Profile</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item onClick={logoutUser}>Logout</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+const mapStateToProps = (state: any) => {
+  return { auth: state.auth };
+};
+export default connect(mapStateToProps)(LoggedInNavbar);

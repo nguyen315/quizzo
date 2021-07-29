38e1654aa55424fd33224c0ae8c6edd3e579a0cf
nguyen315 <nguyen315@github.com>
2021-07-29 22:40:55 +0700

    change filter icon

--- client/src/components/question/FilterBar.tsx
+++ client/src/components/question/FilterBar.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
-import { BsSearch, BsX } from 'react-icons/bs';
+import { BiFilterAlt } from 'react-icons/bi';
 import '../../css/questions/filterBar.css';
 import '../../css/questions/searchBar.css';
 
 const FilterBar = () => {
   return (
     <Form className="d-flex filter-bar-form">
-      <BsSearch className="search-icon" />
+      <BiFilterAlt className="search-icon" />
 
       <input
         type="search"

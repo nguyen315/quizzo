aeaecdb70ae3139645ee097db1ce0c937d69afc7
Julian Le <julianle@Julians-MacBook-Pro.local>
2021-07-29 17:22:56 +0700

    front end of List Rooms

--- client/src/components/room/room.tsx
+++ client/src/components/room/room.tsx
@@ -3,22 +3,54 @@ import { Card } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 
 import '../../css/room/room.css';
+import { Dropdown } from 'react-bootstrap';
 
 const Room = () => {
   return (
     <Card className="room">
       <Card.Body>
-        <Card.Title className="room-question">
+        <Card.Title className="room-title">
           It’s a me-Mario! Thank you for playing my game! It’s a me-Mario! Thank
           you for playing my game!
         </Card.Title>
-        <Card.Text className="room-info">
-          <div>Question ID: {}</div>
-          <div>Tags: {}</div>
-          <div>Total questions: {}</div>
-          <div>Level: {}</div>
-          <div>Time per question: {}</div>
-          <div>Created at: {}</div>
+
+        <Dropdown className="drop-down">
+          <Dropdown.Toggle
+            variant="success"
+            id="dropdown-basic"
+          ></Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Preview</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Edit</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Report</Dropdown.Item>
+            <Dropdown.Item href="#/action-3" id="delete">
+              Delete
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Card.Text className="info">
+          <div>
+            <label className="room-info">Room PIN:</label>
+            <span className="room-info-answer">123456 {}</span>
+          </div>
+          <div>
+            <label className="room-info">Total questions:</label>
+            <span className="room-info-answer">10</span>
+          </div>
+          <div>
+            <label className="room-info">Level:</label>
+            <span className="room-info-answer">hard</span>
+          </div>
+          <div>
+            <label className="room-info">Time per question:</label>
+            <span className="room-info-answer">30s</span>
+          </div>
+          <div>
+            <label className="room-info">Created at:</label>
+            <span className="room-info-answer">30/06/2021</span>
+          </div>
         </Card.Text>
         <Button className="play-button">Play</Button>
         {/* <Button variant="primary">Play</Button> */}
--- client/src/components/room/searchBar.tsx
+++ client/src/components/room/searchBar.tsx
@@ -1,16 +1,24 @@
 import { Form } from 'react-bootstrap';
 import '../../css/room/searchbar.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 const SearchBar = () => {
   return (
     <div className="room-list-search-bar">
       <div id="room-list">Rooms list</div>
-      <Form.Control
-        className="search-bar"
-        size="lg"
-        type="text"
-        placeholder="Search for room title"
-      />
+      <Form.Group className="groupInput">
+        <Form.Label className="iconInput">
+          <FontAwesomeIcon icon={faSearch} />
+        </Form.Label>
+        <Form.Control
+          className="search-bar"
+          size="lg"
+          type="text"
+          placeholder="Search for room title"
+        />
+      </Form.Group>
     </div>
   );
 };

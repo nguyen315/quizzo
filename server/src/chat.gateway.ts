81697faaf90c872e46646c5b483c4a25d0ccec00
superman19993 <73101805+superman19993@users.noreply.github.com>
2021-08-03 14:18:54 +0700

    reformat the code

--- server/src/chat.gateway.ts
+++ server/src/chat.gateway.ts
@@ -85,22 +85,22 @@ export class ChatGateway {
 
   @SubscribeMessage('player-submit')
   async handlePlayerSubmit(@MessageBody() data, @ConnectedSocket() client) {
-
     // calculate the answering time of the player.
     const questionStartTime = new Date(questions[data.roomId].timeStamp);
     const answerTime = new Date(data.timeStamp);
     const thinkingTime = answerTime.getTime() - questionStartTime.getTime();
     // check if the player choose the right answer.
     const questionDetais = await this.questionService.findOne(data.questionId);
-    const correctAnswer= (questionDetais.answers.find(i => i.isCorrect===true)).id;
-    const coeff= Number(data.answerId)===correctAnswer? 1:0;
+    const correctAnswer = questionDetais.answers.find(
+      (i) => i.isCorrect === true
+    ).id;
+    const coeff = Number(data.answerId) === correctAnswer ? 1 : 0;
 
-    let point = coeff*(questions[data.roomId].timeUp * 1000 - thinkingTime);
+    let point = coeff * (questions[data.roomId].timeUp * 1000 - thinkingTime);
     point = point < 0 ? 0 : point;
     // add point of the questions to total score of the player
-    const currentPlayer= players[data.roomId].find(i=> i.id===client.id);
-    currentPlayer.point+= point;
-    
+    const currentPlayer = players[data.roomId].find((i) => i.id === client.id);
+    currentPlayer.point += point;
   }
 
   @SubscribeMessage('host-end-question')

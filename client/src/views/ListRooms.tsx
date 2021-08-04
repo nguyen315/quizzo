47399b4cb2f6145b0e231dc24bcde4159f75519d
Cong Toan Le <congtoanle01@gmail.com>
2021-08-04 13:38:14 +0700

    fix merging

--- client/src/views/ListRooms.tsx
+++ client/src/views/ListRooms.tsx
@@ -1,3 +1,45 @@
+// import React, { useEffect } from 'react';
+// import Question from '../components/question/Question';
+// import { Container } from 'react-bootstrap';
+// import MyNavbar from '../components/layouts/MyNavbar';
+// import SearchBar from '../components/room/searchBar';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { RootState } from '../store/store';
+// import { fetchQuestions } from '../store/slices/questions.slice';
+
+// import { Row, Col, Card } from 'react-bootstrap';
+
+// import Room from '../components/room/room';
+// import AddRoom from '../components/room/AddRoom';
+
+// import '../css/room/room.css';
+// import '../css/room/searchbar.css';
+
+// const ListRooms: React.FC = () => {
+//   return (
+//     <Container fluid>
+//       <MyNavbar />
+//       <SearchBar />
+//       {/* <div>// {content}</div> */}
+
+//       <Row xs={1} md={2} lg={4} className="list-room">
+//         {Array.from({ length: 1 }).map((_, idx) => (
+//           <Col>
+//             <AddRoom />
+//           </Col>
+//         ))}
+//         {Array.from({ length: 8 }).map((_, idx) => (
+//           <Col>
+//             <Room />
+//           </Col>
+//         ))}
+//       </Row>
+//     </Container>
+//   );
+// };
+
+// export default ListRooms;
+
 import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import SearchBar from '../components/room/searchBar';
@@ -15,7 +57,6 @@ import LoggedInNavBar from '../components/layouts/LoggedInNavBar';
 
 const ListRooms: React.FC = () => {
   const rooms = useSelector((state: RootState) => state.rooms.rooms);
-
   const roomsStatus = useSelector((state: RootState) => state.rooms.status);
   const roomsError = useSelector((state: RootState) => state.rooms.error);
 
